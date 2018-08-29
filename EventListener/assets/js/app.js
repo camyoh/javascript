998553a0dabab8daacdf570a0db330ea3d95a95e
Estudiante-4 <estudiante-4@iMac-de-Multimedia-4.local>
2018-08-29 12:22:39 -0500

    agrego archivos

---
+++ EventListener/assets/js/app.js
@@ -0,0 +1,16 @@
+﻿//variables
+
+
+//Event listeners
+EventListener();
+function EventListener() {
+    document.getElementById('formulario').addEventListener('submit', agregarTweet)
+}
+
+
+//funciones
+function agregarTweet(e) {
+    e.preventDefault();
+    const tweet = document.getElementById('tweet').value;
+    console.log(tweet);
+}
